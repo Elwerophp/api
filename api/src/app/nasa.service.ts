3f18f3ef2b7b059ec2d49e79d3b5aebff360ed22
Elwerophp <13056@portalcedi.edu.mx>
2025-01-28 10:14:32 -0600

    pirmer comit

---
+++ api/src/app/nasa.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class NasaService {
+private apiurl = 'https://api.nasa.gov/planetary/apod';
+private apikey = '9y9YgkUeMw5gUfjVxv7yCxQCGTPOE1Rwk1dWqm1kWh';
+
+  constructor(private http:HttpClient) {}
+   getimageoftheday():Observable<any>{
+    return this.http.get(`${this.apiurl}?api_key=${this.apikey}`);
+   }
+
+}
